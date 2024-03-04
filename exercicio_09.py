4a93ec7843558892588c14b04ae7a33daba09fb9
Murilo Sacchi <144244138+murilosacchi@users.noreply.github.com>
2024-03-04 18:24:41 -0300

    Update exercicio_09.py

--- exercicio_09.py
+++ exercicio_09.py
@@ -1,2 +1,15 @@
 import math   
 
+a = float(input('Digite o número A: '))
+b = float(input('Digite o número B: '))
+c = float(input('Digite o número C: '))
+
+if a>0:
+    delta = math.sqrt(b**2-4*a*c)
+    if delta>=0:
+        raiz1 = (-b+delta)/2*a
+        raiz2 = (-b-delta)/2*a
+        print(f'as raízes da equação do 2º grau são {raiz1} e {raiz2}')
+        
+    else:
+        print('Não é uma equação do 2º grau')
