c7a08956a38c0447edd4d923b720c97f0772a52b
Murilo Sacchi <murilo.sacchi1126@gmail.com>
2024-03-27 11:42:06 -0300

    update

--- exercicio_38_corrigir.py
+++ exercicio_38_corrigir.py
@@ -1,19 +1,14 @@
 # Dada uma lista de números, use um for loop para verificar quais são números primos.
 
-import random
-
-lista_numeros = []
+lista_numeros = range(2, 100)
 primos = []
 
-for i in range(10):
-    numero = random.randint(1, 100)
-    lista_numeros.append(numero)
-    for num in lista_numeros:
-        for i in range(2, num):
-            if (num % i) == 0:
+for num in lista_numeros:
+    for i in range(2, num):
+        if (num % i) == 0:
                 break
-        else:
-            primos.append(num)
+    else:
+        primos.append(num)
 
 print(f'Números primos: {primos}')
 
